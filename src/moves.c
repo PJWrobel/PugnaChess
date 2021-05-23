daa0a2500d6e0f39e58cfea72dc70cb8804e90f7
PJWrobel <wrobel.paulj@gmail.com>
2021-05-22 23:17:15 -0600

    about to mess stuff up, changed moves.c

---
+++ src/moves.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include "board.h"
+
+
+void pawnMoves(Board *b)
+{   BitBoard pawns = b->
+}
+
+void moves(BitBoard *returnList, Board *b)
+{   pawnMoves
+}
